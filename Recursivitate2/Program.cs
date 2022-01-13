579850725da39e07add0eef9866901aed1c71d06
Sami00002 <samuel.bodogan@emanuel.ro>
2022-01-13 22:59:59 +0200

    Add project files.

---
+++ Recursivitate2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            Console.WriteLine(Recursiv.Maxim(vector, vector.Length - 1, int.MinValue));
+        }
+    }
+}
---
+++ Recursivitate2/Recursiv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate2
+{
+    class Recursiv
+    {
+        public static int Maxim(int[] vector, int n, int max)
+        {
+            if(n == 0)
+            {
+                if (max >= vector[n])
+                    return max;
+                else
+                    return vector[n];
+            }
+            else
+            {
+                if (max >= vector[n])
+                    return Maxim(vector, n - 1, max);
+                else
+                    return Maxim(vector, n - 1, vector[n]);
+            }
+        }
+    }
+}
